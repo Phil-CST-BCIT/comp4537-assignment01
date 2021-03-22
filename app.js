ad7a1418bd5a812d5cd59281be76787eeb9404bd
Phil <philfeiteng@gmail.com>
2021-03-22 00:29:39 -0700

    add comments

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2')
 const app = express();
 const PORT = process.env.PORT || 5000
 
-
 const pool = mysql.createPool({
     host: helpers.db_host,
     user: helpers.db_user,
@@ -15,14 +14,17 @@ const pool = mysql.createPool({
     queueLimit: 0
   }).promise();
 
+// middle ware for serving static files
 app.use(express.static('public'));
 
+// middle ware for parsing request body in json format
 app.use(express.json())
 
 app.get('/', (req, res) => { 
     res.end(); 
 });
 
+// handles requests for the total number of quizzes in the database
 app.get('/a1/api/lists', (req, res) => {
 
         // query the database and see if there is a quiz
@@ -52,11 +54,12 @@ app.get('/a1/api/lists', (req, res) => {
             res.send(reply);
     
         }) 
-        .catch(() => { console.log("error in line 49")} );
+        .catch(() => { console.log("error in line 57")} );
 
     }
 ); // GET lists 
 
+// handles requests for a specific quiz
 app.get('/a1/api/lists/:id', (req, res) => {
 
     // id is a string
@@ -66,7 +69,7 @@ app.get('/a1/api/lists/:id', (req, res) => {
 
     let str = '';
 
-    // we go to database to get quiz with id
+    // we go to database to get the quiz with a specific id
     const QRY = `SELECT * FROM quizzes as quiz
                             JOIN questions as ques 
                             ON quiz.id = ques.quiz_id 
@@ -105,10 +108,11 @@ app.get('/a1/api/lists/:id', (req, res) => {
 
 
         } )
-        .catch(() => { console.log("Error in line 85"); });
+        .catch(() => { console.log("Error in line 111"); });
 
 }); // GET a quiz with id
 
+// insert one or many questions into the database
 async function insertQuestions(values) {
 
     const qry = `INSERT INTO questions(quiz_id, question, num_answers_id, question_index) VALUES ?`;
@@ -117,6 +121,7 @@ async function insertQuestions(values) {
 
 }
 
+// insert one ore many answers into the database
 async function insertAnswers(values) {
 
     const qry = `INSERT INTO answers(question_id, quiz_id, answer, choice_id, is_solution) VALUES ?;`;
@@ -125,17 +130,16 @@ async function insertAnswers(values) {
 
 }
 
+// handles post requests for storing a quiz
 app.post('/a1/api/questions', (req, res) => {
     
-    // how many questions?
-    // which quiz id?
-
     const body = req.body;
 
     let numOfQuestions = body.length;
 
     const insertQuiz = `INSERT INTO quizzes(num_questions) VALUES(${numOfQuestions});`; 
 
+    // inserts one quiz with num_questions
     pool.execute(insertQuiz)
         .then(() => {
 
@@ -145,34 +149,41 @@ app.post('/a1/api/questions', (req, res) => {
             let quizID = 0;
             let questionID = 0;
 
+            // when insertQuiz succeeds, we check the id of the inserted quiz
             pool.execute(maxQuizID)
                 .then(([data, metadata]) => { 
                 
                     quizID = data[0]['max(id)'];
 
+                    // prepare question values for insert opearation
                     return helpers.buildQuestionValues(quizID, body);
                 
                 })
                 .then((results) => {
+
+                    // when the values are ready, we insert
                     insertQuestions(results);
                 })
                 .then(() => {
 
+                    // get the id of the last inserted qustion
                     pool.execute(maxQuestionID)
                         .then((data, metadata) => { 
 
+                            // the id of the first inserted question
                             questionID = data[0][0]['max(question_id)'] - (numOfQuestions - 1);
 
+                            // prepare values for answers
                             return helpers.buildAnswerValues(questionID ,quizID, body);
         
                         })
                         .then((values) => { insertAnswers(values); }) 
-                        .catch(() => {console.log("Error in line 166")});
+                        .catch(() => {console.log("Error in line 181")});
                 })
-                .catch((err) => console.log("Error in line 168"));
+                .catch((err) => console.log("Error in line 183"));
 
         })
-        .catch(() => console.log("Error in line 171") )
+        .catch(() => console.log("Error in line 186") )
     
 
     res.set({
